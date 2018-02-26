82f20520b2bfdffb87cdc1311dd0fed37aba44a0
Miguel Pardo <migsub77@gmail.com>
2018-02-26 02:25:11 -0800

    test

--- routes/slack/slack.js
+++ routes/slack/slack.js
@@ -4,7 +4,24 @@ const axios = require('axios');
 
 router.route('/invite')
 .post((req, res) => {
-  let token = process.env.SLACK_TOKEN || 'no_token';
+  let token = process.env.SLACK_TOKEN;
+  let user_email = req.query.email;
+  let slack_invite_endpoint = 'https://slack.com/api/users.admin.invite';
+  let query = `email=${user_email}&token=${token}&set_active=true`;
+  let endpoint_url= `${slack_invite_endpoint}?${query}`;
+  console.log(endpoint_url);
+
+    axios.get(endpoint_url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
+    });
+
+});
+.get((req, res) => {
+  let token = process.env.SLACK_TOKEN;
   let user_email = req.query.email;
   let slack_invite_endpoint = 'https://slack.com/api/users.admin.invite';
   let query = `email=${user_email}&token=${token}&set_active=true`;
