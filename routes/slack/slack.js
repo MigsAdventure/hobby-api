9b3e1162b1364df9b4e251ba99df8df7101e2b05
Miguel Pardo <migsub77@gmail.com>
2018-02-28 22:43:59 -0800

    some clean up

--- routes/slack/slack.js
+++ routes/slack/slack.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 
 router.route('/invite')
 .post((req, res) => {
-  let token = process.env.SLACK_TOKEN;
-  let user_email = req.body.email;
-  let slack_invite_endpoint = 'https://slack.com/api/users.admin.invite';
-  let query = `email=${user_email}&token=${token}&set_active=true`;
-  let endpoint_url= `${slack_invite_endpoint}?${query}`;
+  const token = process.env.SLACK_TOKEN;
+  const user_email = req.body.email;
+  const slack_invite_endpoint = 'https://slack.com/api/users.admin.invite';
+  const query = `email=${user_email}&token=${token}&set_active=true`;
+  const endpoint_url= `${slack_invite_endpoint}?${query}`;
 
     axios.get(endpoint_url)
     .then((response) => {
