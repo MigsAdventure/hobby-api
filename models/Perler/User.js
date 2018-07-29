d28339763f1236fc2e60903c1af4354245034954
Miguel Pardo <migsub77@gmail.com>
2018-07-28 20:09:49 -0700

    added models and routes and package for perler site

--- models/Perler/User.js
+++ models/Perler/User.js
@@ -1,29 +1,37 @@
-// // create a model from user
-// const User = require('../schema/global/user');
-//
-// // checks if user exists in db
-// exports.userExists = (authUser, req, cb) => {
-// // look through all current users in db for employee by emp id
-//   User.find({ employee_id: authUser.employeeID })
-//   .then((dbUser) => {
-//     if (dbUser.length) {
-//       return dbUser;
-//     } else {
-//       const is_admin = authUser.sAMAccountName.toLowerCase().match(/pat|jchen|jmaddox|miguelpg|aniculescu/gm) ? true: false;
-//         // create a new user
-//         return User.create({
-//           employee_id: authUser.employeeID,
-//           name: authUser.sAMAccountName,
-//           email: authUser.mail,
-//           //permissions:
-//           is_admin : is_admin,
-//         })
-//     }
-//   })
-//   .then((sendUser) => {
-//     cb(null, sendUser);
-//   })
-//   .catch((err) => {
-//     cb(err);
-//   });
-// };
+// create a model from user
+const User = require('../../schema/Perler/user');
+
+// checks if user exists in db
+const userExists = (auth_name) => {
+    console.log('auth_name', auth_name);
+// look through all current users in db for user by name
+    const users = ['Miguel Pardo', 'Charina'];
+    const allowedUsers = users.includes(auth_name);
+
+    console.log('allowedUSERS:', allowedUsers);
+
+    if (allowedUsers) {
+        return User.find({ name: auth_name })
+            .then((dbUser) => {
+                if (dbUser.length) {
+                    return dbUser;
+                } else {
+                    // create a new user
+                    return User.create({
+                        name: auth_name,
+                    })
+                }
+            })
+            .then((sendUser) => {
+                return sendUser;
+            })
+            .catch((err) => {
+                return err;
+            });
+    } else {
+        console.log('THIS RUNS');
+        return  new Error('USER IS NOW AUTHORIZED');
+    }
+};
+
+module.exports = userExists;
--- routes/perler/perler.js
+++ routes/perler/perler.js
@@ -1,10 +1,25 @@
 "use strict"
 const router = require('express').Router();
+const User = require('../../models/Perler/User');
 
-router.route('/')
-.get((req, res) => {
-  console.log('received');
-res.send('PERLER WORKS');
+
+router.route('/fetch-user-perlers')
+    .get((req, res) => {
+    console.log('req.body');
+
+    res.send('PERLER WORKS');
+  })
+
+    .post((req, res) => {
+        User.userExists(req.body.name)
+            .then((data) => {
+                console.log('this should work');
+                res.send(data);
+            })
+            .catch((err) => {
+                console.log('ERRORS HERE');
+                res.status(400).send(err);
+            });
 });
 
 module.exports = router;
