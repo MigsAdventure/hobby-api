cd62ba426dc76bc2d25c6d8e6b3276ee8805653f
Miguel Guzman <mig@miguels-mbp.lan>
2020-10-22 00:10:10 -0700

    test stock wheel auth

--- routes/stock-wheel/index.js
+++ routes/stock-wheel/index.js
@@ -3,14 +3,18 @@ const router = require('express').Router();
 const axios = require('axios');
 const formidable = require('formidable');
 
+
+
 router.route('/auth')
 /*
 Callback endpoint the TDA app uses.
 https://developer.tdameritrade.com/content/simple-auth-local-apps
 */
+
 .get((req, res) => {
+  console.log('made it to GET')
   var authRequest = {
-    url: 'https://api.tdameritrade.com/v1/oauth2/token',
+    url: `https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${process.env.TD_REDIRECT_URL}&client_id=${process.env.TD_CLIENT_ID}%40AMER.OAUTHAP`,
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -19,20 +23,37 @@ https://developer.tdameritrade.com/content/simple-auth-local-apps
       'grant_type': 'authorization_code',
       'access_type': 'offline',
       'code': req.query.code, // get the code from url
-      'client_id': process.env.CLIENT_ID + "@AMER.OAUTHAP", // this client id comes from config vars
-      'redirect_uri': redirect_uri
     }
   };
   
+  console.log('2nd STEP!!')
   // make the post request
-  request(authRequest, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // parse the tokens
-      var authReply = JSON.parse(body);
-      // to check it's correct, display it
-      res.send(authReply);
+  // request(authRequest, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     // parse the tokens
+  //     var authReply = JSON.parse(body);
+  //     // to check it's correct, display it
+  //     res.send(authReply);
+  //   }
+  // });
+  
+  axios.post(authRequest.url, {
+    headers: {
+      "content-type": "application/json",
+      "Accept": "application/json"
     }
+  })
+  .then((response) => {
+    console.log('MADE IT TO POST');
+    console.log('PARSED!!!: ', JSON.parse(response.data));
+    res.send(response.data);
+  })
+  .catch((err) => {
+    console.log('ERROR', err);
   });
+  
+
+  
 });
 
 module.exports = router;
